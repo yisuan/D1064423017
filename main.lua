36be4861ae111dad30eb9a64cca7dd40b6dcb947
yisuan <44424961+yisuan@users.noreply.github.com>
2018-10-29 19:14:50 +0800

    Revert "Update main.lua"
    This reverts commit 5e3adadbcbf9be5446bd77b6e68c6d86bf964057.

--- main.lua
+++ main.lua
@@ -13,6 +13,3 @@ msgText:setFillColor( 0.2, 0.6, 0.8 )
 
 local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 ) 
-
-local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
-msgText:setFillColor( 0.2, 0.6, 0.8 ) 
